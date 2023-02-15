888b895056c63c3ca7067656efafe7c78b2cc6ff
camilin <camilogamboatoloza@outlook.com>
2023-02-15 14:26:43 -0500

    first commit

--- src/Componentes/Header/index.js
+++ src/Componentes/Header/index.js
@@ -2,6 +2,7 @@ import React,{useContext} from 'react'
 import Logo from "../../imagenes/logo.png";
 import {Link} from "react-router-dom";
 import {DataContext} from "../../context/Dataprovider";
+import {Archivo} from "./ARREGLOS.pdf";
 import Login from "../../imagenes/login-icon.jpg";
 import { useAuth0 } from '@auth0/auth0-react';
 import { LoginButton } from '../Login/Login';
@@ -43,7 +44,7 @@ export const Header = () => {
             <a href="https://login-xfx7.onrender.com">SALIR</a>
         </li>
         <li style={{right:"38rem",fontSize:"2rem",top:"8rem"}}>
-        <a href="./ARREGLOS.pdf" download>
+        <a href={Archivo} download>
         {''} 
         ARREGLOS
       </a>
